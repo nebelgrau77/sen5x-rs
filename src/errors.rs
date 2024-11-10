22df7b23f3153dc2414d3ea83b5b86bf0b098e1a
nebelgrau77 <misiek@gmx.it>
2024-11-10 10:47:50 +0100

    removed thiserror flags from errors

--- src/errors.rs
+++ src/errors.rs
@@ -2,23 +2,25 @@ use embedded_hal as hal;
 use hal::i2c::I2c;
 use sensirion_i2c::i2c;
 
+//use thiserror::Error;
+
 /// SEN5x errors
 #[derive(Debug, PartialEq)]
-#[cfg_attr(feature = "thiserror", derive(thiserror::Error))]
+//#[cfg_attr(feature = "thiserror", derive(thiserror::Error))]
 pub enum Error<E> {
-    #[cfg_attr(feature = "thiserror", error("I2C: {0}"))]
+    //#[cfg_attr(feature = "thiserror", error("I2C: {0}"))]
     /// I²C bus error
     I2c(E),
-    #[cfg_attr(feature = "thiserror", error("CRC"))]
+    //#[cfg_attr(feature = "thiserror", error("CRC"))]
     /// CRC checksum validation failed
     Crc,
-    #[cfg_attr(feature = "thiserror", error("Self Test"))]
+    //#[cfg_attr(feature = "thiserror", error("Self Test"))]
     /// Self-test measure failure
     SelfTest,
-    #[cfg_attr(feature = "thiserror", error("Not Allowed"))]
+    //#[cfg_attr(feature = "thiserror", error("Not Allowed"))]
     /// Not allowed when periodic measurement is running
     NotAllowed,
-    #[cfg_attr(feature = "thiserror", error("Internal"))]
+    //#[cfg_attr(feature = "thiserror", error("Internal"))]
     /// Internal fail
     Internal,
 }
